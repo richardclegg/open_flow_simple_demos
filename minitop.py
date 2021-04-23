b88947e4aebe168ed08eb4f7fbdddb7aeef965f2
Richard G. Clegg <richard@richardclegg.org>
2021-04-23 16:07:05 +0100

    minitop.py

---
+++ minitop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from mininet.topo import Topo
+
+class CPTopology( Topo ):
+    "Simple topology example for computerphile"
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'CPtopology': ( lambda: CPTopology() ) }
